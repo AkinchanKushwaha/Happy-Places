1c4f992a34c9a69501300bc245fc3a7e38923555
AkinchanKushwaha <akinchandroger@gmail.com>
2021-02-02 21:29:03 +0530

    Fixing The Updating Duplication Bug

--- app/src/main/java/com/happyplaces/activities/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/happyplaces/activities/AddHappyPlaceActivity.kt
@@ -138,7 +138,7 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
                     else -> {
 
                         val happyPlaceModel = HappyPlaceModel(
-                                0,
+                                if(mHappyPlaceDetails == null) 0 else mHappyPlaceDetails!!.id,
                                 et_title.text.toString(),
                                 saveImageToInternalStorage.toString(),
                                 et_description.text.toString(),
@@ -149,12 +149,18 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
                         )
 
                         val dbHandler = DatabaseHandler(this)
-
-                        val addHappyPlace = dbHandler.addHappyPlace(happyPlaceModel)
-
-                        if (addHappyPlace > 0) {
-                            setResult(Activity.RESULT_OK)
-                            finish()
+                        if(mHappyPlaceDetails == null){
+                            val addHappyPlace = dbHandler.addHappyPlace(happyPlaceModel)
+                            if (addHappyPlace > 0) {
+                                setResult(Activity.RESULT_OK)
+                                finish()
+                            }
+                        }else{
+                            val updateHappyPlace = dbHandler.updateHappyPlace(happyPlaceModel)
+                            if (updateHappyPlace > 0) {
+                                setResult(Activity.RESULT_OK)
+                                finish()
+                            }
                         }
                     }
                 }
